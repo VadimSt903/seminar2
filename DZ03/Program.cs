70051596b074bb2c6f12c4a7be78581977d56551
Vadim <sg_vadim@mail.ru>
2022-11-26 19:54:02 +0300

    Сделал DZ03

---
+++ DZ03/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введи цифру, обозначающую день недели: ");
+int daynum = Convert.ToInt32 (value: Console.ReadLine());
+
+if (daynum == 6 ^ daynum == 7) 
+{
+  Console.WriteLine("да (это выходной день)");
+}
+  else 
+    if (daynum < 1 ^ daynum > 7) 
+  {
+    Console.WriteLine("это не день недели");
+  }
+  else 
+{
+  Console.WriteLine("нет (это не выходной день)");
+}
