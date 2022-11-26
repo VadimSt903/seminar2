b93ca2906532820c15080961fe8f991de5a295b6
Vadim <sg_vadim@mail.ru>
2022-11-26 19:01:57 +0300

    Сдалал DZ02

---
+++ DZ02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int num = Convert.ToInt32 (value: Console.ReadLine());
+if(num < 100)
+{
+    Console.WriteLine("Нет третьего числа");
+}
+else 
+{  
+    for(; num >= 1000; num /= 10);  
+
+    Console.WriteLine(num % 10);
+}
+
+
