9abbf6a95e1b3bd9390c74d79398326ca6f05469
Vadim <sg_vadim@mail.ru>
2022-11-26 13:36:56 +0300

    Сделал DZ01

---
+++ DZ01/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int num1 = Convert.ToInt32 (value: Console.ReadLine());
+string number2 = Convert.ToString(num1);
+Console.WriteLine("вторая цифра этого числа -> "+number2[1]);
+
